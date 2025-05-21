c4aaf3406047462e7c373e2ca36e4d6ec967be59
Valentin Chaillou <vchaillo@student.42.fr>
2025-05-21 13:56:09 +0200

    add angular app deploy to gh pages

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -17,10 +17,60 @@ export class CalendarComponent implements OnInit {
 
   majority: number = 18;
 
+  user = {
+    id: "1",
+    name: "Valentin Chaillou",
+    nickName: "Onefroza",
+    birth: "10.20.1989",
+    events: [
+      {
+        id: "1",
+        userId: "1",
+        title: "Evènement ponctuel",
+        color: "cornflowerblue",
+        yearStart: 18,
+        yearEnd: 18,
+        weekStart: 40,
+        weekEnd: 40
+      },
+      {
+        id: "2",
+        userId: "1",
+        title: "Evènement période",
+        color: "yellow",
+        yearStart: 28,
+        yearEnd: 35,
+        weekStart: 0,
+        weekEnd: 32
+      }
+    ]
+  };
+
   ngOnInit(): void {
-    this.initCalendar();
+    this.initUser();
+    // this.initCalendar();
+
     // console.log('years : ', this.years);
     console.log('weekNumbers : ', this.weekNumbers);
+  }
+
+  initUser(): void {
+    this.user.events.forEach(event => {
+
+    });
+
+    for (let i = 0; i < 90; i++) {
+      let weeks: Week[] = [];
+
+      for (let j = 1; j <= 52; j++) {
+        let color = (j - 1 === 0 && i % 5 === 0) ? 'coral' : 'white';
+        weeks.push(new Week(color, j, null));
+      }
+
+      this.years[i] = new Year(i, weeks);
+    }
+
+    this.initCalendar
   }
 
   initCalendar(): void {
